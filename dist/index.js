cf20fa0d5d3c2b65c1610baf83d67c196b51bebb
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-11-27 05:18:52 -0400

    fix dist

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
+#!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const svex_1 = require("./commands/svex");
-commander_1.program.command('svex.ts').description('Create your custom api requests handler like axios, but for sveltekit').action(svex_1.runCommand);
+commander_1.program
+  .command("svex.ts")
+  .description(
+    "Create your custom api requests handler like axios, but for sveltekit"
+  )
+  .action(svex_1.runCommand);
 commander_1.program.parse(process.argv);
