5358d96c5ca2366daa1dcd5a07788c020d6496fd
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-11-27 07:47:43 -0400

    formatting code

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,41 +11,48 @@ export const vscodeDebugConfig = {
 
 export const clientCode = {
   url: `
-  import {baseUrl} from "$env/static/private"
-  import type {RequestEvent} from "@sveltejs/kit"
-  import {debuggerInstance} from "$lib/utils/debugger"
+import {baseUrl} from "$env/static/private"
+import type {RequestEvent} from "@sveltejs/kit"
+import {debuggerInstance} from "$lib/utils/debugger"
   `,
   client: `
-    export const client = async (event: RequestEvent, endpoint: string, method: string, raw?: object, headers?: any, debug?: boolean) => {
+import { baseUrl } from "$env/static/private";
+import type { RequestEvent } from "@sveltejs/kit";
+import { debuggerInstance } from "$lib/utils/debugger";
 
-    let body = raw? JSON.stringify(raw):null
+export const client = async (
+  event: RequestEvent,
+  endpoint: string,
+  method: string,
+  raw?: object,
+  headers?: any,
+  debug?: boolean
+) => {
+  let body = raw ? JSON.stringify(raw) : null;
 
-  let res = await fetch(baseUrl + endpoint, { method, body, headers })
+  let res = await fetch(baseUrl + endpoint, { method, body, headers });
 
   if (debug) {
-    await debuggerInstance("CLIENT", raw, res, endpoint, headers, method)
+    await debuggerInstance("CLIENT", raw, res, endpoint, headers, method);
   }
 
   if (res.ok) {
-    let data = await res.json()
-    return { ok: true, status: res.status, data }
+    let data = await res.json();
+    return { ok: true, status: res.status, data };
   } else {
-    let data
+    let data;
     try {
-      data = await res.json()
-
+      data = await res.json();
     } catch (error) {
-      data = undefined
+      data = undefined;
     }
     return {
       ok: false,
       status: res.status,
-      data: JSON.stringify(data)
-    }
+      data: JSON.stringify(data),
+    };
   }
-
-}
-  `,
+};`,
 };
 
 export const debuggerCode = `
@@ -102,14 +109,13 @@ export const debuggerInstance = async (
       status: response.status,
     });
   }
-};
-  `;
+};`;
 
 export const appLocalsCode = {
   inLocals: `
-  interface Locals {
-    svelxios: Client
-  }
+    interface Locals {
+      svelxios: Client
+    }
   `,
   interfaces: `
 interface Response {
@@ -119,11 +125,11 @@ interface Response {
 }
 
 interface Client {
-  GET: (endpoint: string, body?: object, params?: object, headers?: any) => Promise<Response>;
-  POST: (endpoint: string, body?: object, params?: object, headers?: any) => Promise<Response>;
-  PUT: (endpoint: string, body?: object, params?: object, headers?: any) => Promise<Response>;
-  PATCH: (endpoint: string, body?: object, params?: object, headers?: any) => Promise<Response>;
-  DELETE: (endpoint: string, body?: object, params?: object, headers?: any) => Promise<Response>;
+  GET: (endpoint: string, body?: object, headers?: any) => Promise<Response>;
+  POST: (endpoint: string, body?: object, headers?: any) => Promise<Response>;
+  PUT: (endpoint: string, body?: object, headers?: any) => Promise<Response>;
+  PATCH: (endpoint: string, body?: object, headers?: any) => Promise<Response>;
+  DELETE: (endpoint: string, body?: object, headers?: any) => Promise<Response>;
 }
   `,
 };
